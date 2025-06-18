69369eb1ad653040628f5dadee35bc88a2d9034b
Juan Esteban Acevedo <124696997+JuanesAcevedoP@users.noreply.github.com>
2025-06-18 16:22:01 -0500

    console.log en uploadroutes

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -4,11 +4,13 @@ const { upload } = require('../config/multerConfig');
 const { protect } = require('../middlewares/authMiddleware');
 
 router.post('/', protect, upload.single('image'), (req, res) => {
+  console.log("Archivo recibido:", req.file);
+
   if (!req.file || !req.file.key || !req.file.bucket) {
     return res.status(400).json({ message: 'No se pudo subir la imagen' });
   }
 
-  const imageUrl = `https://gateway.eu1.storjshare.io/${req.file.bucket}/${req.file.key}`;
+  const imageUrl = `https://gateway.storjshare.io/${req.file.bucket}/${req.file.key}`;
 
   res.status(200).json({ url: imageUrl });
 });
